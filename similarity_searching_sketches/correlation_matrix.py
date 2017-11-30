b257031094f1231dfafb6eee2f4c44d754c1124c
matejkvassay <matejkvassay5@gmail.com>
2017-11-30 17:10:16 +0100

    uncorrelated bit selection experiments

---
+++ similarity_searching_sketches/correlation_matrix.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+def compute_corr_mtx(sketch_matrix):
+    """
+    Computes Correlation Matrix from given Sketch matrix.
+    :param sketch_matrix: Numpy matrix, 1 row - 1 sketch
+    :return: Pearson correlation matrix
+    """
+    corr_mtx = np.absolute(np.corrcoef(sketch_matrix.T))
+    np.fill_diagonal(corr_mtx, 0.0)
+    return corr_mtx
+
+
+def get_upper_triangle_values(mtx, include=None, exclude=None):
+    """
+    Get values from upper triangle of Matrix.
+    :param mtx: Numpy matrix
+    :param include: If not None only columns with these indices will be included.
+    :param exclude: If not None all columns with these indices will be excluded.
+    :return: Array of values from upper triangle of given matrix.
+    """
+    if include is None and exclude is None:
+        u = np.triu_indices(n=mtx.shape[0], m=mtx.shape[1], k=1)
+        return mtx[u]
+    if include is not None:
+        selection = mtx[include].T[include]
+        u = np.triu_indices(n=selection.shape[0], m=selection.shape[1], k=1)
+        return selection[u]
+    if exclude is not None:
+        mask = np.ones(mtx.shape[0], dtype=bool)
+        mask[exclude] = 0
+        selection = mtx[mask].T[mask]
+        u = np.triu_indices(n=selection.shape[0], m=selection.shape[1], k=1)
+        return selection[u]
+    raise ValueError('Choose only indices to either include or exclude, not both.')
+
+
+def get_one_vs_all_corrs(corr_mtx, col_idx):
+    """
+    Returns list of 1 vs All correlations for given bit index.
+    :param corr_mtx: Sketch correlation matrix.
+    :param col_idx: Index of bit to compute correlations from
+    :return: Array of correlations except for indentity correlation.
+    """
+    return np.delete(corr_mtx[:, col_idx], col_idx)
---
+++ similarity_searching_sketches/uncorrelated_bit_selection.py
@@ -0,0 +1,73 @@
+import numpy as np
+from similarity_searching_sketches.correlation_matrix import get_upper_triangle_values
+
+def select_random_subset(corr_mtx, p, k):
+    """
+    Selects k random p-subsets of bits of Sketch Matrix and selects the one resulting in lowest mean correlation
+    of upper triangle in given Sketch Correlation Matrix.
+    :param corr_mtx: Sketch Correlation Matrix
+    :param p: Desired Sketch length
+    :param k: Number of subsets to evaluate
+    :return: Tuple (array of selected column indices, mean correlation, correlation st.dev)
+    """
+    if p < 2:
+        return None, None, None
+    rang = np.arange(corr_mtx.shape[0])
+    subsets = np.array([np.random.choice(rang, size=p, replace=False) for _ in range(k)])
+    best_subset = None
+    best_corr_mean = 1.0
+    best_corr_std = 0.0
+    for indices in subsets:
+        upper_tria = get_upper_triangle_values(corr_mtx, include=indices)
+        corr_mean = np.mean(upper_tria)
+        if corr_mean <= best_corr_mean:
+            best_corr_mean = corr_mean
+            best_corr_std = np.std(upper_tria)
+            best_subset = indices
+    return np.array(best_subset), best_corr_mean, best_corr_std
+
+
+def fast_minimal_correlation_columns(corr_mtx, p):
+    """
+    Implementation of FMCC algorithm. Selects low correlated subset of bits.
+    :param corr_mtx: Sketch Correlation Matrix
+    :param p: Desired Sketch length
+    :return: Tuple (array of selected column indices, mean correlation, correlation st.dev)
+    """
+    if p < 2:
+        return None, None, None
+    else:
+        selected_cols = np.sum(corr_mtx, axis=1).argsort()[:p]
+        upper_tria = get_upper_triangle_values(corr_mtx, include=selected_cols)
+        return selected_cols, np.mean(upper_tria), np.std(upper_tria)
+
+
+def greedy_minimal_correlation_columns(corr_mtx, p):
+    """
+    Implementation of GMCC algorithm. Selects low correlated subset of bits.
+    :param corr_mtx: Sketch Correlation Matrix
+    :param p: Desired Sketch length
+    :return: Tuple (array of selected column indices, mean correlation, correlation st.dev)
+    """
+    if p < 2:
+        return None, None, None
+    least_correlated = np.argmin(np.sum(corr_mtx, axis=1))
+    indices = []  # to keep them sorted
+    indices_lookup = set()  # to speed up search for membership
+    indices.append(least_correlated)
+    indices_lookup.add(least_correlated)
+    while len(indices) < p:
+        best_i = None
+        best_mean_corr = 1.0
+        best_corr_std = 1.0
+        for i in range(corr_mtx.shape[0]):
+            if i not in indices_lookup:
+                upper_tria = get_upper_triangle_values(corr_mtx, include=list(indices) + [i])
+                corr_mean = np.mean(upper_tria)
+                if corr_mean < best_mean_corr:
+                    best_i = i
+                    best_mean_corr = corr_mean
+                    best_corr_std = np.std(upper_tria)
+        indices_lookup.add(best_i)
+        indices.append(best_i)
+    return np.array(indices), best_mean_corr, best_corr_std
